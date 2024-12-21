e14ea3562cca073b6492784086b192990b7fdba4
Adetokunbo Andrew <Adetokunboandrew2@gmail.com>
2024-12-22 00:24:56 +0100

    express-tutorial

--- server.js
+++ server.js
@@ -6,9 +6,15 @@ const app = express();
 app.get('/',(req,res) =>{
     res.send('<h1 style="color:red;">Hello World</h1>')
 })
+
+app.get("/form",(req,res)=>{
+    res.sendFile(__dirname + "/files/form.html")
+})
+
 app.get('/greet',(req,res) =>{
-    console.log(req.query);
-    
-    res.send('Good Morning!')
+    let person = "Guest"
+    if(req.query.person) 
+        person = req.query.person
+    res.send("Greetings from here! " + person)
 })
 app.listen(3000)
