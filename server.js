c702466a0822e2731dc588d4ab50f5d6867d6575
Adetokunbo Andrew <Adetokunboandrew2@gmail.com>
2024-12-24 22:43:14 +0100

    express-tutorial

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const express = require('express')
 
 const app = express();
 
+
+
+app.use(express.urlencoded({extended: true}))
 app.get('/',(req,res) =>{
     res.send('<h1 style="color:red;">Hello World</h1>')
 })
